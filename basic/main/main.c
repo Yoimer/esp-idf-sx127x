282419393bdc80aa16f1eb8be58d151099710c4b
Yoimer Román <ydrf19@gmail.com>
2023-03-24 01:17:22 -0400

    sends temperatures values from a queue

--- basic/main/main.c
+++ basic/main/main.c
@@ -2,8 +2,13 @@
 #include <inttypes.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 #include "esp_log.h"
 #include "lora.h"
+
+TaskHandle_t myTaskHandle = NULL;
+TaskHandle_t myTaskHandle2 = NULL;
+QueueHandle_t queue;
 
 /* Note: ESP32 don't support temperature sensor */
 
@@ -20,32 +25,17 @@ void app_main(void)
 
 static const char *TAG = "TempSensor";
 
-// void tempsensor_example(void *arg)
-// {
-//     // Initialize touch pad peripheral, it will start a timer to run a filter
-//     ESP_LOGI(TAG, "Initializing Temperature sensor");
-//     float tsens_out;
-//     temp_sensor_config_t temp_sensor = TSENS_CONFIG_DEFAULT();
-//     temp_sensor_get_config(&temp_sensor);
-//     ESP_LOGI(TAG, "default dac %d, clk_div %d", temp_sensor.dac_offset, temp_sensor.clk_div);
-//     temp_sensor.dac_offset = TSENS_DAC_DEFAULT; // DEFAULT: range:-10℃ ~  80℃, error < 1℃.
-//     temp_sensor_set_config(temp_sensor);
-//     temp_sensor_start();
-//     ESP_LOGI(TAG, "Temperature sensor started");
-//     while (1) {
-//         vTaskDelay(1000 / portTICK_RATE_MS);
-//         temp_sensor_read_celsius(&tsens_out);
-//         ESP_LOGI(TAG, "Temperature out celsius %f°C", tsens_out);
-//     }
-//     vTaskDelete(NULL);
-// }
-
-
-#if CONFIG_SENDER
-void task_tx(void *pvParameters)
+void tempsensor_example(void *arg)
 {
+    char txBuffer[50];
+    queue = xQueueCreate(5, sizeof(txBuffer)); 
+    if (queue == 0)
+    {
+     printf("Failed to create queue= %p\n", queue);
+    }
+	
 	// Initialize touch pad peripheral, it will start a timer to run a filter
-    ESP_LOGI(TAG, "Initializing Temperature sensor....");
+    ESP_LOGI(TAG, "Initializing Temperature sensor");
     float tsens_out;
     temp_sensor_config_t temp_sensor = TSENS_CONFIG_DEFAULT();
     temp_sensor_get_config(&temp_sensor);
@@ -53,21 +43,54 @@ void task_tx(void *pvParameters)
     temp_sensor.dac_offset = TSENS_DAC_DEFAULT; // DEFAULT: range:-10℃ ~  80℃, error < 1℃.
     temp_sensor_set_config(temp_sensor);
     temp_sensor_start();
-	ESP_LOGI(TAG, "Temperature sensor started...");
+    ESP_LOGI(TAG, "Temperature sensor started");
+    while (1) {
+        vTaskDelay(1000 / portTICK_RATE_MS);
+        temp_sensor_read_celsius(&tsens_out);
+        ESP_LOGI(TAG, "Temperature out celsius %f°C", tsens_out);
 
+		sprintf(txBuffer, "%.6f", tsens_out);
+		xQueueSend(queue, (void*)txBuffer, (TickType_t)0);
+
+    }
+    vTaskDelete(NULL);
+}
+
+
+#if CONFIG_SENDER
+void task_tx(void *pvParameters)
+{
 	ESP_LOGI(pcTaskGetName(NULL), "Start");
 	uint8_t buf[256]; // Maximum Payload size of SX1276/77/78/79 is 255
 	while(1) {
-		temp_sensor_read_celsius(&tsens_out);
-    	ESP_LOGI(TAG, "Temperature out celsius %f°C", tsens_out);
 		TickType_t nowTick = xTaskGetTickCount();
-		int send_len = sprintf((char *)buf, "%.6f", tsens_out, nowTick);
-		lora_send_packet(buf, send_len);
-		ESP_LOGI(pcTaskGetName(NULL), "%d byte packet sent...", send_len);
-		vTaskDelay(pdMS_TO_TICKS(2000));
+		vTaskDelay(pdMS_TO_TICKS(1000));
+
+		if( xQueueReceive(queue, &(buf), (TickType_t)5)){
+			printf("Received data from queue ==== %s\n", buf);
+		}
+
+		lora_send_packet(buf, 256);
+
 	} // end while
 }
 #endif // CONFIG_SENDER
+
+#if CONFIG_RECEIVER
+void task_rx(void *pvParameters)
+{
+	ESP_LOGI(pcTaskGetName(NULL), "Start");
+	uint8_t buf[256]; // Maximum Payload size of SX1276/77/78/79 is 255
+	while(1) {
+		lora_receive(); // put into receive mode
+		if (lora_received()) {
+			int receive_len = lora_receive_packet(buf, sizeof(buf));
+			ESP_LOGI(pcTaskGetName(NULL), "%d byte packet received:[%.*s]", receive_len, receive_len, buf);
+		} // end if
+		vTaskDelay(1); // Avoid WatchDog alerts
+	} // end while
+}
+#endif // CONFIG_RECEIVER
 
 void app_main()
 {
@@ -126,8 +149,8 @@ void app_main()
 	ESP_LOGI(pcTaskGetName(NULL), "spreading_factor=%d", sf);
 
 #if CONFIG_SENDER
-	xTaskCreate(&task_tx, "task_tx", 1024*3, NULL, 5, NULL);
-	// xTaskCreate(tempsensor_example, "temp", 2048, NULL, 5, NULL);
+	xTaskCreate(&task_tx, "task_tx", 1024*3, NULL, 5, &myTaskHandle2);
+	xTaskCreate(tempsensor_example, "temp", 2048, NULL, 5, &myTaskHandle);
 #endif
 #if CONFIG_RECEIVER
 	xTaskCreate(&task_rx, "task_rx", 1024*3, NULL, 5, NULL);
